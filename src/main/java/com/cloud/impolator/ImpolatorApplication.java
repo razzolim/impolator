642019981f8dfb7b448e11ea3fa88675883361c5
Maicon Fang <maicon.fang@gmail.com>
2021-02-25 20:49:25 -0300

    initial project implementation

---
+++ src/main/java/com/cloud/impolator/ImpolatorApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.impolator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpolatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpolatorApplication.class, args);
+	}
+
+}
