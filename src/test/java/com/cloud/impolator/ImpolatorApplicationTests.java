7c1d03189fde222157c09118e2e70832458f6acb
Renan Azzolim <azzolimrenan2@gmail.com>
2021-02-25 21:24:08 -0300

    adjustments
    - included circleCI
    -included one test vo validate CI

--- src/test/java/com/cloud/impolator/ImpolatorApplicationTests.java
+++ src/test/java/com/cloud/impolator/ImpolatorApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cloud.impolator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class ImpolatorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
